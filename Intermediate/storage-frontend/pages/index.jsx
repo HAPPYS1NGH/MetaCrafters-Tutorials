28cb08e69d01047e25535f43276745eacca2c87b
HAPPYS1NGH <harpreetsingh140503@gmail.com>
2023-03-30 12:35:25 +0530

    V1

--- Intermediate/storage-frontend/pages/index.jsx
+++ Intermediate/storage-frontend/pages/index.jsx
@@ -6,16 +6,17 @@ import { ethers } from "ethers";
 import { ConnectButton } from '@rainbow-me/rainbowkit';
 // react hooks for setting and changing states of variables
 import { useEffect, useState } from 'react';
-import {useProvider , useAccount , useContractRead} from 'wagmi'
+import { useProvider, useAccount, useContractRead } from 'wagmi'
+import { useContractWrite, usePrepareContractWrite , useWaitForTransaction} from 'wagmi'
 
 export default function Home() {
   // env variables are initalised
   // contractAddress is deployed smart contract addressed 
   const contractAddress = process.env.CONTRACT_ADDRESS;
   // application binary interface is something that defines structure of smart contract deployed.
-  const abi = process.env.ABI;
+  const abi = process.env.ABI
 
-  
+
   // response from read operation is stored in the below variable
   const [storedNumber, setStoredNumber] = useState();
 
@@ -23,59 +24,77 @@ export default function Home() {
   const [enteredNumber, setEnteredNumber] = useState(0);
   const provider = useProvider();
   const signer = useAccount();
+
   const { data, isError, isLoading } = useContractRead({
     address: contractAddress,
     abi: abi,
     functionName: 'readNum',
+    watch:true,
+  })
+
+  function handleForm(e) {
+    setEnteredNumber(e.target.value)
+  }
+
+
+  async function readNumber() {
+    setStoredNumber(parseInt(data))
+    console.log(parseInt(data))
+  }
+
+
+  const { config } = usePrepareContractWrite({
+    address: contractAddress,
+    abi: abi,
+    functionName: 'writeNum',
+    args: [enteredNumber]
+  })
+  const { data: writeNumFunc, isLoading: writeNumLoading, isSuccess, write } = useContractWrite({
+    ...config,
+    
     onSuccess(data) {
       console.log('Success', data)
     },
-    onError(error) {
-      console.log('Error', error)
-    },
   })
-  
-  
-  async function readNumber(){
-    // eslint-disable-next-line react-hooks/rules-of-hooks
-    setStoredNumber(parseInt(data))
-  console.log(parseInt(data))
+  const waitForTransaction = useWaitForTransaction({
+    hash: writeNumFunc?.hash,
+  })
+
+  async function writeNumber() {
+    await write();
+    console.log(writeNumFunc)
+    console.log(waitForTransaction)
   }
-  
-  async function writeNumber(){
-    try {
-      const smartContract = new ethers.Contract(contractAddress, abi, provider);
-      const contractWithSigner = smartContract.connect(signer);
 
-      // interact with the methods in smart contract as it's a write operation, we need to invoke the transation usinf .wait()
-      const writeNumTX = await contractWithSigner.writeNum(enteredNumber);
-      const response = await writeNumTX.wait()
-      console.log(await response)
+  useEffect(()=>{
+    readNumber()
+      console.log("Wait for transaction "+ waitForTransaction)
+      console.log("Use Contract Write for transaction "+ useContractWrite)
+      console.log("Use Contract Read for transaction "+ useContractRead)
+  }, [waitForTransaction , useContractWrite , useContractRead])
 
-      alert(`Number stored successfully ${enteredNumber}`)   
-      return
-
-    } catch (error) {
-      alert(error)
-      setStoreLoader(false)
-      return
-    }
-  }
-  
 
   return (
     <div className='m-6 space-y-4'>
       <div className="flex p-4">
-      <h1 className="text-gray-700 text-3xl font-bold">
-        Storage Frontend Demo
-      </h1>
-      <div className="ml-auto">
-      <ConnectButton />
-      </div>
+        <h1 className="text-gray-700 text-3xl font-bold">
+          Storage Frontend Demo
+        </h1>
+        <div className="ml-auto">
+          <ConnectButton />
+        </div>
       </div>
 
       <h3>This action retrieves the saved number from smart contract. (i.e Read Operation)</h3>
       <button onClick={readNumber}>ReadNumber : {storedNumber}</button>
-      </div>
+      <h3>This will do Write function</h3>
+      <input type="text" value={enteredNumber} onChange={handleForm} />
+      <br />
+        <button className="bg-blue-500 hover:bg-blue-700 text-white font-bold py-2 px-4 rounded" disabled={!write} onClick={writeNumber}>
+          Write
+        </button>
+        {writeNumLoading && <div>Check Wallet</div>}
+        {waitForTransaction.isLoading && <div>Transacting ..... </div>}
+    </div>
   )
 }
